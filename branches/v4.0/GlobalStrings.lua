db46e12cb1897e23dbc66119a10f189bec8452f9
vandesdelca32 <vandesdelca32@0964c68e-58e1-43e3-a0b2-5d3c5f19ab01>
2010-11-13 04:40:10 +0000

    Begin working on 4.0 patch for CombatMusic Will eventually migrate this to it's own addon/repos

---
+++ branches/v4.0/GlobalStrings.lua
@@ -0,0 +1,48 @@
+﻿--[[
+------------------------------------------------------------------------
+	Project: Van32sCombatMusic
+	File: GlobalStrings revision @file-revision@
+	Date: @project-date-iso@
+	Purpose: Global Strings, non-localized.
+	Credits: Code written by Vandesdelca32
+
+	Copyright (c) 2010 Vandesdelca32
+
+		This file is part of Van32sCombatMusic.
+
+ Van32sCombatMusic is free software: you can redistribute it and/or 
+modify it under the terms of the GNU General Public License as published 
+by the Free Software Foundation, either version 3 of the License, or (at 
+your option) any later version. 
+
+ Van32sCombatMusic is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General 
+Public License for more details. 
+
+ You should have received a copy of the GNU General Public License along 
+with Van32sCombatMusic. If not, see <http://www.gnu.org/licenses/>. 
+
+------------------------------------------------------------------------
+]]
+
+local addonName, CM = ...
+
+CM["TITLE"] = "CombatMusic"
+
+local CSTR = "|cff%02X%02X%02X" -- Color code string
+CM["COLORS"] = {
+	END = "|r",
+	ERROR = format(CSTR, 230, 10, 10),
+	GREEN = format(CSTR, 10, 230, 10),
+	DEBUG = format(CSTR, 165, 205, 160), 
+	ORANG = format(CSTR, 255, 75, 0),
+	TITLE = format(CSTR, 175, 150, 255),
+}
+CM["VERSION"] = GetAddOnMetadata(addonName, "Version") .. "r" .. (tostring("@project-revision@") or "DEV")
+CM["SV_COMPATIBLE"] = "2"
+
+CM["DEBUG"] = false
+--@alpha@
+CM["DEBUG"] = true
+--@end-alpha@
---
+++ branches/v4.0/Locale/enUS.lua
@@ -0,0 +1,45 @@
+﻿--[[
+------------------------------------------------------------------------
+	Project: Van32sCombatMusic
+	File: enUS revision @file-revision@
+	Date: @project-date-iso@
+	Purpose: enUS string localizations
+	Credits: Code written by Vandesdelca32
+
+	Copyright (c) 2010 Vandesdelca32
+
+		This file is part of Van32sCombatMusic.
+
+ Van32sCombatMusic is free software: you can redistribute it and/or 
+modify it under the terms of the GNU General Public License as published 
+by the Free Software Foundation, either version 3 of the License, or (at 
+your option) any later version. 
+
+ Van32sCombatMusic is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General 
+Public License for more details. 
+
+ You should have received a copy of the GNU General Public License along 
+with Van32sCombatMusic. If not, see <http://www.gnu.org/licenses/>. 
+
+------------------------------------------------------------------------
+]]
+
+local addonName, CM = ...
+
+local L = LibStub("AceLocale-3.0"):NewLocale(CM.TITLE, "enUS", true)
+
+if L then
+	L["ERROR"] = CM.COLORS.ERROR .. "[ERROR]" .. CM.COLORS.END
+	L["DEBUG"] = CM.COLORS.DEBUG .. "<DEBUG>" .. CM.COLORS.END
+	L["Addon Loaded"] = CM.TITLE .. " version " .. CM.VERSION .. " loaded successfuly!"
+	L["Show Once Help"] = "Type /cm help for a list of slash commands!"
+	L["Configuration Loaded"] = "Core settings loaded."
+	L["BossList Loaded"] = "Custom songlist loaded."
+	L["Errors"] = {
+		["No Boss List"] = "Couldn't find a custom songlist, resetting the songlist instead.",
+		["No Settings"] = "Couldn't find the settings, resetting to default",
+		["Out of Date Settings"] = "Settings are out of date, restoring defaults.",
+	}
+end
