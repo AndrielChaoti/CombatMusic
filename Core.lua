9269d255a62be1725f777b14cb73a71e527712e0
vandesdelca32 <vandesdelca32@0964c68e-58e1-43e3-a0b2-5d3c5f19ab01>
2012-03-02 00:51:53 +0000

    Update copyright notices.

--- Core.lua
+++ Core.lua
@@ -6,7 +6,7 @@
 	Purpose: The reusable, essential functions that any addon needs.
 	Credits: Code written by Vandesdelca32
 
-    Copyright (C) 2011  Vandesdelca32
+    Copyright (C) 2010-2012 Vandesdelca32
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
--- Strings.lua
+++ Strings.lua
@@ -6,7 +6,7 @@
 	Purpose: The English string localization for CombatMusic.
 	Credits: Code written by Vandesdelca32
 	
-    Copyright (C) 2011  Vandesdelca32
+    Copyright (C) 2010-2012 Vandesdelca32
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
