2b23cc9d105254790134fa851049125fee172513
vandesdelca32 <vandesdelca32@0964c68e-58e1-43e3-a0b2-5d3c5f19ab01>
2012-11-09 09:11:00 +0000

    Some changes to the Dev version for the next major release. Add ignore for shortcut files.

---
+++ branches/dev/core.lua
@@ -0,0 +1,9 @@
+﻿--[[
+	Project: Van32sCombatMusic
+	File: core, revision @file-revision@
+	Date: @file-date-iso@
+	Purpose: The heart of CombatMusic.
+	Credits: Code written by Vandesdelca32
+	
+    Copyright ©2010-2012 Vandesdelca32
+]]
