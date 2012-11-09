b22957112f75ae7fe318b8bb8ac2c9ddfb2ef83f
vandesdelca32 <vandesdelca32@0964c68e-58e1-43e3-a0b2-5d3c5f19ab01>
2012-11-09 09:13:39 +0000

    devstuff

---
+++ branches/dev/init.lua
@@ -0,0 +1,15 @@
+--[[
+	Project: Van32sCombatMusic
+	File: init, revision @file-revision@
+	Date: @file-date-iso@
+	Purpose: Basic initialization
+	Credits: Code written by Vandesdelca32
+	
+    Copyright ©2010-2012 Vandesdelca32
+]]
+
+local name, addOn = ...
+
+addOn[1] = {}
+addOn[2] = LibStub:GetLibrary("AceLocale-3.0"):GetLocale(name)
+addOn[3] = 
---
+++ branches/dev/locale/enUS.lua
@@ -0,0 +1,16 @@
+﻿--[[
+	Project: Van32sCombatMusic
+	File: enUS, revision @file-revision@
+	Date: @file-date-iso@
+	Purpose: Localization (English US).
+	Credits: Code written by Vandesdelca32
+	
+    Copyright ©2010-2012 Vandesdelca32
+]]
+
+local addonName = ...
+local L = LibStub:GetLibrary("AceLocale-3.0"):NewLocale(addonName, "enUS", true)
+
+if L then
+
+end
