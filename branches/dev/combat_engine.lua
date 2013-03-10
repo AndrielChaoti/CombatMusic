4a373afcb1a9c3e2849ff9a1a66412bd901d8a63
vandesdelca32 <vandesdelca32@0964c68e-58e1-43e3-a0b2-5d3c5f19ab01>
2013-03-10 12:44:16 +0000

    Delete Dev branch

--- branches/dev/combat_engine.lua
+++
@@ -1,9 +0,0 @@
---[[
-	Project: Van32sCombatMusic
-	File: combat_engine, revision @file-revision@
-	Date: @file-date-iso@
-	Purpose: The code that handles the music.
-	Credits: Code written by Vandesdelca32
-	
-    Copyright ©2010-2012 Vandesdelca32
-]]
