660e020b4bd6328c43a74bc0cc5821e78ad3b670
vandesdelca32 <vandesdelca32@0964c68e-58e1-43e3-a0b2-5d3c5f19ab01>
2016-10-25 23:31:09 +0000

    Tagging latest stable build

---
+++ tags/release_v5.4.26/src/locale/enUS.lua
@@ -0,0 +1,20 @@
+--[[
+
+	Project Name: CombatMusic
+	Author: Vandesdelca32
+
+	File: enUS.lua
+	Purpose: English Localization
+
+	Copyright © 2016 by Vandesdelca32
+	All rights reserved. The contents of this file 	or any portion thereof
+	may not be reproduced without the express written permission of the
+	publisher.
+]]
+
+local addonName = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(addonName, "enUS", true, true)
+
+if L then
+	--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true)@
+end
