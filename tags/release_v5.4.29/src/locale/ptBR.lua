ff5b01eead1ae5de02c16f1f3897422fd97dbab9
Vandesdelca32 <Vandesdelca32@0964c68e-58e1-43e3-a0b2-5d3c5f19ab01>
2016-12-31 23:44:49 +0000

    Tagging latest stable build

---
+++ tags/release_v5.4.29/src/locale/ptBR.lua
@@ -0,0 +1,7 @@
+local addonName = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(addonName, "ptBR")
+
+if L then
+	--@localization(locale="ptBR", handle-unlocalized="comment", format="lua_additive_table", same-key-is-true=true)@
+end
+
