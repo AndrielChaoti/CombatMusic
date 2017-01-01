bfa5691b6803bd7e866d8c0869df43196be92dbf
Vandesdelca32 <Vandesdelca32@0964c68e-58e1-43e3-a0b2-5d3c5f19ab01>
2017-01-01 01:12:01 +0000

    Tagging latest stable build

---
+++ tags/release_v5.4.30/src/locale/ruRU.lua
@@ -0,0 +1,6 @@
+local addonName = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(addonName, "ruRU")
+
+if L then
+	--@localization(locale="ruRU", handle-unlocalized="comment", format="lua_additive_table", same-key-is-true=true)@
+end
